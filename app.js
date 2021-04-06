1f39bab09f2140a56e4220f031637f6b5d7f7c67
Ibrahim <ibrahimzerlin@hotmail.com>
2021-04-06 12:48:24 -0500

    module for the function

--- app.js
+++ app.js
@@ -1,55 +1,15 @@
-
-/*
-
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-      console.log(profileDataArr[i]);
-    }
-  
-    console.log('================');
-  
-    // Is the same as this...
-    profileDataArr.forEach((profileItem) => {
-      console.log(profileItem)
-    });
-};
-
-printProfileData(profileDataArgs);
-
-*/
 const fs = require('fs');
+const generatePage = require ('./src/page-template.js');
 
 
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-const name = profileDataArgs[0];
-const github = profileDataArgs[1];
+const [name, github] = profileDataArgs;
 
-const generatePage = (userName, githubName) => {
-  return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
 
-  <body>
-    <h1>${name}</h1>
-    <h2><a href="https://github.com/${github}">Github</a></h2>
-  </body>
-  </html>
-  `;
-};
 
-console.log(generatePage(name, github));
 
 fs.writeFile('index.html', generatePage(name, github), err => {
-  if (err) throw err;
+  if (err) throw new Error(err);
 
   console.log('Portfolio complete! Check out index.html to see the output!');
 });
