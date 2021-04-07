067bdd6868be137cde1824fc1b1f447bb6ec4eba
Ibrahim <ibrahimzerlin@hotmail.com>
2021-04-07 09:45:27 -0500

    html creation with filter and map

--- app.js
+++ app.js
@@ -1,22 +1,10 @@
 
 const inquirer = require("inquirer");
 
-/*
 const fs = require('fs');
 const generatePage = require ('./src/page-template.js');
 
 
-const pageHTML = generatePage(name, github);
-
-
-
-
-fs.writeFile('index.html', pageHTML, err => {
-  if (err) throw new Error(err);
-
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
-*/
 const  promptUser = () => {
   return inquirer.prompt([
     {
@@ -147,11 +135,18 @@ const promptProject = portfolioData => {
   });
 };
 
-
 promptUser()
   .then(promptProject)
   .then(portfolioData => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    
+    fs.writeFile('index.html', pageHTML, err => {
+    if (err) throw new Error(err);
+
+    console.log('Portfolio complete! Check out index.html to see the output!');
+    });
+    
+
   });
   
 
